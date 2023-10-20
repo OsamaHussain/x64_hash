34da832f7d47f95bf7e0735f32e8a1a60e3694ef
OsamaHussain <osamahussain897@gmail.com>
2023-10-20 06:55:31 +0500

    OK

---
+++ run.js
@@ -0,0 +1,18 @@
+// Developer: Osama Hussain
+// GitHub:    https://github.com/OsamaHussain
+
+// One-Way Hashing Algorithm x64_hash That Uses Simple Method to Hash Data
+function x64_hash(input) {
+  let hash = 0n;
+  for (let i = 0; i < input.length; i++) {
+    const charCode = BigInt(input.charCodeAt(i));
+    hash = hash * 31n + charCode;
+    hash = hash & 0xffffffffffffffffn;
+  }
+  const hashString = hash.toString(16).padStart(16, "0");
+  return hashString;
+}
+
+// Result
+const result = x64_hash("My_Password_123");
+console.log(result);
